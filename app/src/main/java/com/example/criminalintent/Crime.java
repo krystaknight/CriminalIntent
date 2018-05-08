af98246366ed3634b7038a4504d6ee228e2eb39a
Krysta Knight <klknight23@gmail.com>
2018-05-08 16:27:58 -0500

    Add crime fragment

--- app/src/main/java/com/example/criminalintent/Crime.java
+++ app/src/main/java/com/example/criminalintent/Crime.java
@@ -3,12 +3,22 @@ package com.example.criminalintent;
 import java.util.UUID;
 
 public class Crime {
-
     private UUID mId;
     private String mTitle;
 
-    public Crime(){
-        // Generate unique identifier
+    public Crime() {
         mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
     }
 }
